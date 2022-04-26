a6d90af3bfc02eba9e6f3f433542ae71c637dca1
alexkomaro <alex.komaro@gmail.com>
2022-04-26 20:12:29 +0300

    app ready

--- app.py
+++ app.py
@@ -9,11 +9,11 @@ def compute():
     result = ''
     error = dict()
     if request.method == 'POST':
-        data = request.form
+        ds = request.form
         try:
-            data1 = {k:[float(v)] for k,v in data.items()}
+            df = {k:[float(v)] for k,v in ds.items()}
         except ValueError:
-            for k,v in data.items():
+            for k,v in ds.items():
                 try:
                     float(v)
                 except ValueError:
@@ -21,11 +21,11 @@ def compute():
                     error['value'] = v
                     break
         if not error:
-            input = pd.DataFrame(data=data1)
-            result = model.predict(input)[0][0]
+            input = pd.DataFrame(data=df)
+            result = model.predict(input).round(3)[0][0]
     return render_template('index.html', result=result, error=error)
 
 
 if __name__ == '__main__':
     model = load_model('model/model/')
-    app.run(host='localhost', port=8081)
+    app.run(host='localhost', port=8080)
